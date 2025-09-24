8292fd6f3a4221a995810b6ddcbb612e88a8a11c
SIJULY <104404467+SIJULY@users.noreply.github.com>
2025-09-24 20:49:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,8 +43,8 @@ check_and_install_deps() {
     # 检查 Docker Compose
     if ! command -v docker-compose &> /dev/null; then
         echo "未检测到 Docker Compose，正在安装..."
-        sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
+        curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        chmod +x /usr/local/bin/docker-compose
     fi
     
     echo -e "${GREEN}所有依赖已准备就绪。${PLAIN}"
